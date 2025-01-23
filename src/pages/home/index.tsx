29fda6077a399065c23397267345ed169b59ff0f
TFscript <developertiagof@gmail.com>
2025-01-23 10:01:11 -0300

    styles(criando a pagina de home)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,10 +4,10 @@ import brainPad from "../../assets/img/brainpad.svg";
 const Home = () => {
   return (
     <section className={styles.homeContainer}>
-      <img src={brainPad} alt="" className={styles.homeImage} />
-      <h1 className={styles.homeTitle}>Crie Notas Inteligentes com</h1>
+      
+      <h1 className={styles.title}>Salve suas <span>ideias</span> no <span className={styles.spanPad}>NotesPad</span></h1>
 
-      <h1 className={styles.subTitle}>NotesPad</h1>
+      <img src={brainPad} alt="" className={styles.homeImage} />
     </section>
   );
 };
