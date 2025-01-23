71ece95d95fd8b43dd505dc4891850837d125cfc
TFscript <developertiagof@gmail.com>
2025-01-23 07:51:40 -0300

    feat(configurando rotas)

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+// instalando react router
+import { BrowserRouter, Route, Routes } from "react-router";
 
 const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
 );
