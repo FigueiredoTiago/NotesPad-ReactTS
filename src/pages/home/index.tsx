812ad04b9968abd1a1e605d7ad365b0da4e8b2de
TFscript <developertiagof@gmail.com>
2025-02-18 16:18:19 -0300

    styles(responsividade na home adc)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,4 @@
 import styles from "./styles.module.css";
-import brainPad from "../../assets/img/brainpad.svg";
 import LoginModal from "../../components/LoginModal/index";
 import RegisteModal from "../../components/RegisterModal/index";
 import { ToastContainer } from "react-toastify";
@@ -23,14 +22,6 @@ const Home = () => {
         <LoginModal />
         <RegisteModal />
       </nav>
-
-      <div className={styles.home_content}>
-        <h1 className={styles.title}>
-          Salve suas <span>ideias</span> no{" "}
-          <span className={styles.spanPad}>NotesPad</span>
-        </h1>
-        <img src={brainPad} alt="" className={styles.homeImage} />
-      </div>
 
       <ToastContainer />
     </section>
